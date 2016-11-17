d12e12ef074872d810ca1a94c9074a93c79d0d6e
Mike Allred <mikejallred@gmail.com>
2016-11-17 22:31:21 +0000

    Small things.

--- test/controllers/resume_controller_test.rb
+++ test/controllers/resume_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ResumeControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+     assert true
+   end
 end
