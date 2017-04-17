f6d704dcab36d9702958189923860c173782dfba
Mike Allred <mikejallred@gmail.com>
2017-04-17 18:26:45 +0000

    Placeholder.

--- src/app/app.js
+++ src/app/app.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { render } from 'react-dom';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
 
-class HelloWorld extends React.Component {
-  render() {
-    return <p>Hi</p>;
-  }
-}
+let store = createStore(todoApp)
 
 render(
-  <div>Howdy</div>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
