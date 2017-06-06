cd9dc7ace1209f1595f2f8ba673d8b236b3557cd
Mike Allred <mikejallred@gmail.com>
2017-06-06 14:09:07 -0400

    Fixing test.

--- test/controllers/brain_leader_controller_test.rb
+++ test/controllers/brain_leader_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class BrainLeaderControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get brain_leader_index_url
+    #get brain_leader_index_url
+    get :index
     assert_response :success
   end
 
