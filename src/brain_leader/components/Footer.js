d5b3948936390a9749dd82e08bad65a939a612af
Mike Allred <mikejallred@gmail.com>
2017-06-14 05:29:12 -0400

    brainleader: styling

--- src/brain_leader/components/Footer.js
+++ src/brain_leader/components/Footer.js
@@ -7,9 +7,11 @@ import FilterLink from '../containers/FilterLink'
 
 const Footer = () => (
   <Row>
-    <Col xsOffset={4} md={4}>
+    <Col xs={4} />
+    <Col xs={4}>
       <center>BRAIN LEADER<p/>(<a style={{color: '#c0c0c0'}} href="http://www.sjgames.com/dice/zombiedice/">Zombie Dice</a> Scorekeeper)</center>
     </Col>
+    <Col xs={4} />
   </Row>
 )
 
