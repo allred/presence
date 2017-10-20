70c5693dc44cf35e9fd4e49a5a827662172306c2
Mike Allred <mikejallred@gmail.com>
2017-10-19 22:06:13 -0400

    improvements to destroyers.

--- app/controllers/destroyers_controller.rb
+++ app/controllers/destroyers_controller.rb
@@ -3,8 +3,14 @@ class DestroyersController < ApplicationController
     path_file_html = Rails.root + "public/report_destroyers.html"
     @data_html = File.read(path_file_html)
 
-    %w% <html> </html> <body> </body> %.each do |w|
-      @data_html.gsub!(w, '')
-    end
+    @data_html.gsub!(%r%
+      ^<html>$
+      |^</html>$
+      |^<body>$
+      |^</body>$
+    %x, '')
+
+    @data_html.gsub!('<table>', '<table class="table table-striped">')
+    @data_html.gsub!(/(http.*?)</, '<a href="\1" target="_blank">\1</a><')
   end
 end
