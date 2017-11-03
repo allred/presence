c566e8533d981540bf3738d53d1e7abf5532566e
Mike Allred <mikejallred@gmail.com>
2017-11-03 14:35:23 -0400

    Cleanup destroyers page.

--- app/controllers/destroyers_controller.rb
+++ app/controllers/destroyers_controller.rb
@@ -1,6 +1,7 @@
 class DestroyersController < ApplicationController
   def index
     path_file_html = Rails.root + "public/report_destroyers.html"
+    @mtime = File.mtime(path_file_html)
     @data_html = File.read(path_file_html)
 
     @data_html.gsub!(%r%
