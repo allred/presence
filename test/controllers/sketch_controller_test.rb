8688536b1c1028588ee1e3c26ad0cd32105e846e
Mike Allred <mikejallred@gmail.com>
2017-12-20 18:02:37 -0500

    Fix test.

--- test/controllers/sketch_controller_test.rb
+++ test/controllers/sketch_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class SketchControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get sketch_index_url
+    get sketch_url
     assert_response :success
   end
 
